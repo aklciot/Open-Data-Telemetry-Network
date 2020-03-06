f74c13ad071d8acf2183f894dfb0c364af0d95fe
aklciot <39285415+aklciot@users.noreply.github.com>
2020-03-06 16:08:10 +1300

    Update TTGW_LoRaGW_Repeater_2.3.2.ino

--- TTGW_LoRaGW_Repeater_2.3.2.ino
+++ TTGW_LoRaGW_Repeater_2.3.2.ino
@@ -52,6 +52,30 @@
 //  https://techtutorialsx.com/2017/07/26/esp32-arduino-setting-a-soft-ap/
 //  *******************************************************************************************************
 
+/*
+The Non-Profit Open Software License version 3.0 (NPOSL-3.0)
+
+Copyright (c) 2019 Innovate Auckland
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+
 
 // Paramaters that are likely to change are stored in a separate file.
 // This source code file should not change between rebuilds

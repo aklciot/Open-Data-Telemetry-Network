4f82211383da06f30abe792f2a0b331fde9e4368
aklciot <39285415+aklciot@users.noreply.github.com>
2021-02-25 20:40:23 +1300

    Update param.h

--- param.h
+++ param.h
@@ -2,7 +2,7 @@
 #define GWID "TTGWLB1"        // Gateway mode ID
 char* MonID = "TTMonLB1";     // Monitor mode ID must start with TTMon to trigger auto-reply from a receiving gateway
 char* RPID = "RP8";           // Repeater mode ID used to prevent endless message loops between repeaters
-char* SNodeID = "TT-RP8";     // Node ID used in repeater mode status message. Please see the manual for more explanations of the monitor and repater operating modes
+char* SNodeID = "TT-RP8";     // Node ID used in repeater mode status message. Please see the manual for more explanations of the monitor and repeater operating modes
 
 #define MQTT_SERVER       "mqtt.innovateauckland.nz"
 #define MQTT_SERVERPORT   1883
